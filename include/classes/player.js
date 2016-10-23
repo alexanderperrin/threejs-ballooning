73484d7c7332c0e534190f65a07e7e9f4a4a2606
Alexander Perrin <contact@alexperrin.com>
2016-10-23 23:19:57 +1100

    Added rope/scarf simulation. Has some issues with low framerates however. Consider using better motion integration.

--- include/classes/player.js
+++ include/classes/player.js
@@ -1,7 +1,10 @@
+import Mathf from './mathf';
+
 const FLIGHT_SPEED = 16;
 const BANK_SPEED = 0.5;
 const SPIN_SPEED = 1;
 const SIDEWAYS_SPEED = 5;
+const FLAG_ADJUST_SPEED = 15;
 
 class Player extends THREE.Object3D {
   constructor() {
@@ -11,10 +14,27 @@ class Player extends THREE.Object3D {
     this.velocity = new THREE.Vector3();
     this.bankVelocity = 0.0;
     this.rotation.set( 0, 0, 0, 'ZXY' );
+    this.scarf = null;
     this.gridPos = {
       x: 0,
       y: 0
     };
+    this.initScarf();
+  }
+
+  initScarf() {
+    let material = new THREE.LineBasicMaterial( {
+      color: 0x994400
+    } );
+    let geometry = new THREE.Geometry();
+    for ( let i = 0; i < 20; ++i ) {
+      geometry.vertices.push( new THREE.Vector3( this.position.x, this.position.y, this.position.z + i * -5 ) );
+    }
+    this.scarf = {
+      renderer: new THREE.Line( geometry, material ),
+      points: geometry.vertices
+    };
+    window.flight.scene.add( this.scarf.renderer );
   }
 
   update() {
@@ -40,9 +60,24 @@ class Player extends THREE.Object3D {
     this.rotation.z += this.bankVelocity * dt;
     this.rotation.z -= Math.sign( this.rotation.z ) * Math.pow( this.rotation.z, 2 ) * dt * 5;
 
-    // this.rotation.y = Math.sin( window.flight.time * 0.2 );
-    // this.rotation.z += dt * 10;
+    for ( let i = 0; i < this.scarf.points.length; ++i ) {
+      let points = this.scarf.points;
+      if ( i === 0 ) {
+        // Position first scarf point at balloon position
+        points[ i ].set( this.position.x, this.position.y - 3, this.position.z );
+        this.scarf.renderer.geometry.verticesNeedUpdate = true;
+      } else {
+        let p1 = points[ i ];
+        let p2 = points[ i - 1 ];
+        p1.x = Mathf.moveTowards( p1.x, p2.x, FLAG_ADJUST_SPEED * dt * Math.abs( p1.x - p2.x ) );
+        p1.y = Mathf.moveTowards( p1.y, p2.y, FLAG_ADJUST_SPEED * dt * Math.abs( p1.y - p2.y ) );
+        p1.z = Mathf.moveTowards( p1.z, p2.z, FLAG_ADJUST_SPEED * dt * Math.abs( p1.z - p2.z ) );
+        p1.x += Math.sin( p1.z * 0.2 ) * 0.1 / ( i + 1 );
+      }
+    }
+    this.scarf.renderer.geometry.computeBoundingBox();
+    this.scarf.renderer.geometry.computeBoundingSphere();
   }
 }
 
-export default Player;
+export default Player;;
