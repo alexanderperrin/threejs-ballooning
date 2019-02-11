0e94333ab80d40615906a3475e587d0d2b9665fd
Alexander Perrin <contact@alexperrin.com>
2019-02-11 23:31:21 +1100

    Add production configuation

--- webpack.common.js
+++ webpack.common.js
@@ -4,18 +4,12 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-  mode: "development",
   entry: {
     app: "./src/index.js",
   },
-  devtool: "inline-source-map",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    contentBase: "./public",
-    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin(["dist"]),
---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  plugins: [
+    new MinifyPlugin(),
+    new CopyWebpackPlugin([
+      { from: "public", to: "dist", ignore: ["index.html"] },
+    ]),
+  ],
+});
